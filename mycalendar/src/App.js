657bf56b5192d4ee67a41d9f56eba4122b38a4c1
hyip8000 <hyip@purdue.edu>
2018-04-15 21:59:55 -0400

    Trying to google calendar api rn

--- mycalendar/src/App.js
+++ mycalendar/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch, Route, NavLink } from 'react-router-dom'
+import Calendar from './Calendar'
 import './App.css';
 
 class App extends Component {
@@ -7,12 +8,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+            <h3>Ain't no calendar like</h3>
+            <h1>My Calendar</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <component={Calendar}>
       </div>
     );
   }
