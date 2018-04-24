0927bed0fcedabfdb962b82302e447c47f85ed96
Khushali Upadhyaya <kupadhya@purdue.edu>
2018-04-23 21:11:32 -0400

    working on the check mark

--- mycalendar/src/AddEvent.js
+++ mycalendar/src/AddEvent.js
@@ -8,20 +8,20 @@ class AddEvent extends Component {
 
     }
 
-//JAVASCRIPT CODE GOES HERE
+    //JAVASCRIPT CODE GOES HERE
 
-createClose() {
-    var myNodelist = document.getElementsByTagName("LI");
-    var i;
-    for(i = 0; i < myNodelist.length; i++) {
-        var span = document.createElement("SPAN");
-        var txt = document.createTextNode("\u00D7");
-        span.className = "close";
-        span.appendChild(txt);
-        myNodelist[i].appendChild(span);
+    createClose() {
+        var myNodelist = document.getElementsByTagName("LI");
+        var i;
+        for (i = 0; i < myNodelist.length; i++) {
+            var span = document.createElement("SPAN");
+            var txt = document.createTextNode("\u00D7");
+            span.className = "close";
+            span.appendChild(txt);
+            myNodelist[i].appendChild(span);
 
+        }
     }
-}
 
     hideClose() {
         var close = document.getElementsByClassName("close");
@@ -60,118 +60,99 @@ createClose() {
         span.className = "close";
         span.appendChild(txt);
         li.appendChild(span);
-    }
 
+        var close = document.getElementsByClassName("close");
+        var i;
+        for (i = 0; i < close.length; i++) {
+            close[i].onclick = function () {
+                var div = this.parentElement;
+                div.style.display = "none";
 
-newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if(inputValue === '') {
-        alert("Task Field Empty!");
-    }
-    document.getElementById("myUL").appendChild(li);
-    document.getElementById("myInput").value = " ";
-
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-
-    var close = document.getElementsByClassName("close");
-    var i;
-    for(i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-            var div = this.parentElement;
-            div.style.display = "none";
-
+            }
         }
     }
-}
 
     render() {
         return (
             <div>
-              
+
                 <div className="AddEvent"></div>
 
 
                 {/*<head>*/}
-                    <link href="./style.css" type="text/css" rel="stylesheet" />
+                <link href="./style.css" type="text/css" rel="stylesheet" />
 
-                    <meta charSet="utf-8" />
-                    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-                    <title>My Calendar</title>
-                    <meta name="description" content="" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta charSet="utf-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <title>My Calendar</title>
+                <meta name="description" content="" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
 
                 {/*</head>*/}
 
                 {/*<body>*/}
-                    <div className="row">
-                        <div className="medium-8 column"></div>
-                        
-                        <h2><center>Select Date:</center></h2>
-                        <form id="date-form">
-                            <center>
-                                <select name="Month" id="selectMonth" >
-                                    <option value="month">Month</option>
-                                    <option value="january">January</option>
-                                    <option value="february">February</option>
-                                    <option value="march">March</option>
-                                    <option value="april">April</option>
-                                    <option value="may">May</option>
-                                    <option value="june">June</option>
-                                    <option value="july">July</option>
-                                    <option value="august">August</option>
-                                    <option value="september">September</option>
-                                    <option value="october">October</option>
-                                    <option value="november">November</option>
-                                    <option value="december">December</option>
-                                </select>
+                <div className="row">
+                    <div className="medium-8 column"></div>
+
+                    <h2><center>Select Date:</center></h2>
+                    <form id="date-form">
+                        <center>
+                            <select name="Month" id="selectMonth" >
+                                <option value="month">Month</option>
+                                <option value="january">January</option>
+                                <option value="february">February</option>
+                                <option value="march">March</option>
+                                <option value="april">April</option>
+                                <option value="may">May</option>
+                                <option value="june">June</option>
+                                <option value="july">July</option>
+                                <option value="august">August</option>
+                                <option value="september">September</option>
+                                <option value="october">October</option>
+                                <option value="november">November</option>
+                                <option value="december">December</option>
+                            </select>
 
 
-                                <input id="selectDay" placeholder="Day" type="number" name="day" min="1" max="31" />
-                                <select name="Year" id="selectYear">
-                                    <option value="year">Year</option>
-                                    <option value="2018">2018</option>
-                                </select>
-                            </center>
-                        </form>
+                            <input id="selectDay" placeholder="Day" type="number" name="day" min="1" max="31" />
+                            <select name="Year" id="selectYear">
+                                <option value="year">Year</option>
+                                <option value="2018">2018</option>
+                            </select>
+                        </center>
+                    </form>
 
 
 
-                        <h2><center>Select Time:</center></h2>
-                        <form id="time-form">
-                            <center>
-                                <input type="time" id="selectTime" name="time" />
-                            </center>
-                        </form>
+                    <h2><center>Select Time:</center></h2>
+                    <form id="time-form">
+                        <center>
+                            <input type="time" id="selectTime" name="time" />
+                        </center>
+                    </form>
 
 
-                        <div id="myDIV" className="header">
-                            <h2><center>To Do List:</center></h2>
-                            <center>
-                                <form id="inForm">
-                                    <input className="inputBox" type="text" id="myInput" placeholder="Add New Task" />
-                                </form></center>
-                            <button
-                              onClick={(e) => this.newElement(e)}
-                                //onClick= "newElement()"
-                                className="addBtn"
-                                type="submit"
-                            >Add</button>
-                        </div>
-
-                        <ul id="myUL" className="ul-todo"></ul>
-
+                    <div id="myDIV" className="header">
+                        <h2><center>To Do List:</center></h2>
+                        <center>
+                            <form id="inForm">
+                                <input className="inputBox" type="text" id="myInput" placeholder="Add New Task" />
+                            </form></center>
+                        <button
+                            onClick={(e) => this.newElement(e)}
+                            //onClick= "newElement()"
+                            className="addBtn"
+                            type="submit"
+                        >Add</button>
                     </div>
 
+                    <ul id="myUL" className="ul-todo"></ul>
 
-                    <script src="app.js"></script>
-                    <script src="https://apis.google.com/js/api.js"></script>
+                </div>
+
+
+                <script src="app.js"></script>
+                <script src="https://apis.google.com/js/api.js"></script>
 
                 {/*</body>*/}
             </div>
