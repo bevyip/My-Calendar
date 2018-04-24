5df8283a35fbe7fd372b9296652acd9ff60f22e0
Khushali Upadhyaya <kupadhya@purdue.edu>
2018-04-23 23:23:10 -0400

    stored the date, event and time, still need to fix the x

--- mycalendar/src/AddEvent.js
+++ mycalendar/src/AddEvent.js
@@ -49,12 +49,50 @@ class AddEvent extends Component {
         var inputValue = document.getElementById("myInput").value;
 
         /* I NEED THE VALUES FROM TIME AND DATE TOO */ 
-        makeApiCall(inputValue);
+        //makeApiCall(inputValue);
+        //trying to store date and time below:
+        //day month and year are separate
+        //////////////
+        var inputDay = document.getElementById("selectDay").value;
+        var inputMonth = document.getElementById("selectMonth").value;
+        var inputYear = document.getElementById("selectYear").value;
+        var inputTime = document.getElementById("selectTime").value;
+
+        var varday = document.createTextNode(inputDay);
+        var varmonth = document.createTextNode(inputMonth);
+        var varyear = document.createTextNode(inputYear);
+        var vartime = document.createTextNode(inputTime);
+        /////////////
 
         var t = document.createTextNode(inputValue);
         li.appendChild(t);
+
+        ////////////
+        li.appendChild (document.createTextNode("  --  "));
+        li.appendChild(varmonth);
+        li.appendChild (document.createTextNode("  --  "));
+        li.appendChild(varday);
+        li.appendChild (document.createTextNode("  --  "));
+        li.appendChild(varyear);
+        li.appendChild (document.createTextNode("  --  "));
+        li.appendChild(vartime);
+
+        if (inputDay === '') {
+            alert("Day Field Empty!");
+        }
+        if (inputMonth === '') {
+            alert("Month Field Empty!");
+        }
+        if (inputYear === '') {
+            alert("Year Field Empty!");
+        }
+        if (inputTime === '') {
+            alert("Time Field Empty!");
+        }
+        ////////////
+
         if (inputValue === '') {
-            alert("Task Field Empty!");
+            alert("Event Field Empty!");
         }
         document.getElementById("myUL").appendChild(li);
         document.getElementById("myInput").value = " ";
@@ -137,10 +175,10 @@ class AddEvent extends Component {
 
 
                         <div id="myDIV" className="header">
-                            <h2><center>To Do List:</center></h2>
+                            <h2><center>Events:</center></h2>
                             <center>
                                 <form id="inForm">
-                                    <input className="inputBox" type="text" id="myInput" placeholder="Add New Task" />
+                                    <input className="inputBox" type="text" id="myInput" placeholder="Add New Event" />
                                 </form></center>
                             <button
                               onClick={(e) => this.newElement(e)}
